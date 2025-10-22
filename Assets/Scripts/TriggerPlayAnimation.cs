c329cd47141efc516eebb1ca402d97d03511c438
Arianna Cutler <arianna.eunice@gmail.com>
2025-10-22 13:40:18 -0300

    Changed some Materials

--- Assets/Scripts/TriggerPlayAnimation.cs
+++ Assets/Scripts/TriggerPlayAnimation.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class TriggerPlayAnimation : MonoBehaviour
 {
     public GameObject AnimatedObject;
-
+   
     private Animator _animator;
-
+   
+    
     private void Start()
     {
         _animator = AnimatedObject.GetComponent<Animator>();
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
         _animator.enabled = true;
+       
     }
 
     void OnTriggerExit(Collider other)
     {
         _animator.enabled = false;
+       
     }
 
 
